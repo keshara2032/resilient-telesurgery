2e7716a6d39d4115d8b2a11fad0ec6461c2acd66
Seyed Hamid Reza Roodabeh <ydq9ag@virginia.edu>
2023-06-20 06:02:26 -0400

    some refactoring, added future trajectory data to the dataset class

--- dataset.py
+++ dataset.py
@@ -78,25 +78,69 @@ class LOUO_Dataset(Dataset):
     def collate_fn(batch, device=torch.device("cuda" if torch.cuda.is_available() else "cpu")):
         X = []
         Y = []
+        Future_Y = []
         P = []
-        for x, y, p in batch:
+        for x, y, yy, p in batch:
             X.append(x)
             Y.append(y)
+            Future_Y.append(yy)
             P.append(p)
         X = np.array(X)
         Y = np.array(Y)
+        Future_Y = np.array(Future_Y)
         P = np.array(P)
 
         x_batch = torch.from_numpy(X)
         y_batch = torch.from_numpy(Y)
+        yy_batch = torch.from_numpy(Future_Y)
         p_batch = torch.from_numpy(P)
         
         x_batch = x_batch.to(torch.float32)
         y_batch = y_batch.to(torch.float32)
+        yy_batch = yy_batch.to(torch.float32)
         p_batch = p_batch.to(torch.float32)
 
         x_batch = x_batch.to(device)
         y_batch = y_batch.to(device)
+        yy_batch = yy_batch.to(device)
         p_batch = p_batch.to(device)
 
-        return (x_batch, y_batch, p_batch)                    
+        return (x_batch, y_batch, yy_batch, p_batch)
+
+
+def get_dataloaders(task: str,
+                    subject_id_to_exclude: str,
+                    observation_window: int,
+                    prediction_window: int,
+                    batch_size: int):
+    
+    from typing import List
+    import os
+    from functools import partial
+    import torch
+
+    from torch.utils.data import DataLoader
+    from dataset import LOUO_Dataset
+    from datagen import tasks
+    
+
+    def _get_files_except_user(task, data_path, subject_id_to_exclude: int) -> List[str]:
+        assert(task in tasks)
+        files = os.listdir(data_path)
+        csv_files = [p for p in files if p.endswith(".csv")]
+        except_user = [os.path.join(data_path, p) for p in csv_files if not p.startswith(f"{task}_S0{subject_id_to_exclude}")]
+        user = [os.path.join(data_path, p) for p in csv_files if p.startswith(f"{task}_S0{subject_id_to_exclude}")]
+        return except_user, user 
+
+
+    # building train and validation datasets and dataloaders
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    data_path = os.path.join("ProcessedDatasets", task)
+    train_files_path, valid_files_path = _get_files_except_user(task, data_path, subject_id_to_exclude)
+    train_dataset = LOUO_Dataset(train_files_path, observation_window, prediction_window, onehot=True)
+    valid_dataset = LOUO_Dataset(valid_files_path, observation_window, prediction_window, onehot=True)
+
+    train_dataloader = DataLoader(train_dataset, shuffle=True, batch_size=batch_size, collate_fn=partial(LOUO_Dataset.collate_fn, device=device))
+    valid_dataloader = DataLoader(valid_dataset, shuffle=False, batch_size=batch_size, collate_fn=partial(LOUO_Dataset.collate_fn, device=device), drop_last=True) 
+
+    return train_dataloader, valid_dataloader                  
