953aed88bb569244492a9c1a0e9f448ac9d66721
Hamid Roodabeh <hr.roodabeh@gmail.com>
2023-07-26 15:06:59 -0400

    deleted old model folder

--- model.py
+++ model.py
@@ -179,7 +179,7 @@ class RecognitionModel(nn.Module):
         return self.transformer.decoder(x, memory, tgt_mask)
 
 
-class DirectRecognitionModel(nn.Module):
+class DirectTransformerRecognitionModel(nn.Module):
     def __init__(self,
                  encoder_input_dim: int, # dimension of the input to the encoder
                  num_encoder_layers: int,
@@ -210,6 +210,45 @@ class DirectRecognitionModel(nn.Module):
             encoder_layers, num_layers=num_encoder_layers
         )
 
+        # output layer    
+        self.fc_output = nn.Linear(emb_size, tgt_vocab_size)
+
+    def forward(self, src: Tensor):
+        
+        # src transformation
+        src_emb = self.encoder_positional_encoding(src) # add positional encoding to the kinematics data
+        src_emb = self.encoder_input_transform(src_emb)
+        if self.activation:
+            src_emb = self.activation(src_emb)
+
+        # encoder
+        encoded = self.transformer_encoder(src_emb)
+
+        # output
+        out = self.fc_output(encoded)
+
+        return out
+    
+
+class DirectRNNRecognitionModel(nn.Module):
+    def __init__(self,
+                 encoder_input_dim: int, # dimension of the input to the encoder
+                 num_encoder_layers: int,
+                 emb_size: int,
+                 tgt_vocab_size: int, # number of output classes
+                 max_len: int, # maximum length of the encoder input
+                 dropout: float = 0.1,
+                 activation=torch.nn.ReLU()): # activation function of the input dim matching linear layers
+        super(DirectRNNRecognitionModel, self).__init__()
+
+        # parameters
+        self.activation = activation
+        
+        # encoder input transformation to model dimension
+        self.encoder_input_transform = torch.nn.Linear(in_features=encoder_input_dim, out_features=emb_size)
+
+        # lstm encoder
+        
         # output layer    
         self.fc_output = nn.Linear(emb_size, tgt_vocab_size)
 
