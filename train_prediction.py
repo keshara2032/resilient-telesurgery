823e34f05d5b06c536ebc6e2c64bf132bc60d75b
Hamid Roodabeh <hr.roodabeh@gmail.com>
2023-08-31 18:36:23 -0400

    prediction pipeline

--- train_prediction.py
+++ train_prediction.py
@@ -5,282 +5,103 @@ import torch
 import torch.nn.functional as F
 import numpy as np
 import pandas as pd
-from sklearn.metrics import classification_report
 from timeit import default_timer as timer
-from model import RecognitionModel, DirectRecognitionModel, ScheduledOptim, get_tgt_mask
-from utils import get_classification_report, visualize_gesture_ts, get_dataloaders
+from utils import get_dataloaders
+from datagen import kinematic_feature_names, kinematic_feature_names_jigsaws, kinematic_feature_names_jigsaws_no_rot_ps, class_names, all_class_names, state_variables
+from models.utils import plot_bars
+
+import warnings
+warnings.filterwarnings('ignore')
 
 
+# ------------------------------------- Functions ----------------------------------
+def train_model(model, optimizer, train_dataloader):
+    pass
 
-# Data Params -------------------------------------------------------------------------------------------------
-tasks = ["Needle_Passing", "Suturing", "Knot_Tying"]
-class_names = {
-    "Peg_Transfer": ["S1", "S2", "S3", "S4", "S5", "S6", "S7"],
-    "Suturing": ['G1', 'G2', 'G3', 'G4', 'G5', 'G6', 'G8', 'G9', 'G10', 'G11'],
-    "Knot_Tyring": ['G1', 'G11', 'G12', 'G13', 'G14', 'G15'],
-    "Needle_Passing": ["G1", 'G2', 'G3', 'G4', 'G5', 'G6', 'G8', 'G9', 'G10', 'G11']
-}
-all_class_names = ["G1", 'G2', 'G3', 'G4', 'G5', 'G6', 'G8', 'G9', 'G10', 'G11', 'G12', 'G13', 'G14', 'G15']
+def eval_model(model, valid_dataloader):
+    pass
 
-feature_names = [ "PSML_position_x", "PSML_position_y", "PSML_position_z", \
-            "PSML_velocity_x", "PSML_velocity_y", "PSML_velocity_z", \
-            "PSML_orientation_x", "PSML_orientation_y", "PSML_orientation_z", "PSML_orientation_w", \
-            "PSML_gripper_angle", \
-            "PSMR_position_x", "PSMR_position_y", "PSMR_position_z", \
-            "PSMR_velocity_x", "PSMR_velocity_y", "PSMR_velocity_z", \
-            "PSMR_orientation_x", "PSMR_orientation_y", "PSMR_orientation_z", "PSMR_orientation_w", \
-            "PSMR_gripper_angle"]
+
+def save_artifacts(model, train_accuracy, valid_accuracy, train_losses, valid_losses):
+    pass
+
+### -------------------------- DATA -----------------------------------------------------
+tasks = ["Suturing"]
+Features = kinematic_feature_names_jigsaws[38:] + state_variables #kinematic features + state variable features
+# Features = kinematic_feature_names_jigsaws_no_rot_ps + state_variables
 
 one_hot = True
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-observation_window = 30
-prediction_window = 40
+observation_window = 10
+prediction_window = 10
 batch_size = 64
-user_left_out = 2
+subject_id_to_exclude = 2
 cast = True
-train_dataloader, valid_dataloader = get_dataloaders(tasks,
-                                                     user_left_out,
-                                                     observation_window,
-                                                     prediction_window,
-                                                     batch_size,
-                                                     one_hot,
-                                                     class_names = all_class_names,
-                                                     feature_names = feature_names,
-                                                     cast = cast)
+include_image_features = False
+normalizer = '' # ('standardization', 'min-max', 'power', '')
+step = 3 # 10 Hz
 
-print("datasets lengths: ", len(train_dataloader.dataset), len(valid_dataloader.dataset))
-print("X shape: ", train_dataloader.dataset.X.shape, valid_dataloader.dataset.X.shape)
-print("Y shape: ", train_dataloader.dataset.Y.shape, valid_dataloader.dataset.Y.shape)
+for subject_id_to_exclude in [2, 3, 4, 5, 6, 7, 8, 9]:
+    train_dataloader, valid_dataloader = get_dataloaders(tasks=tasks,
+                                                        subject_id_to_exclude=subject_id_to_exclude,
+                                                        observation_window=observation_window,
+                                                        prediction_window=prediction_window,
+                                                        batch_size=batch_size,
+                                                        one_hot=one_hot,
+                                                        class_names = class_names['Suturing'],
+                                                        feature_names = Features,
+                                                        include_image_features = include_image_features,
+                                                        cast = cast,
+                                                        normalizer = normalizer,
+                                                        step=step)
 
-# loader generator aragement: (src, src_image, tgt, future_gesture, future_kinematics)
-print("Obs Kinematics Shape: ", train_dataloader.dataset[0][0].shape) 
-print("Obs Target Shape: ", train_dataloader.dataset[0][2].shape)
-print("Future Target Shape: ", train_dataloader.dataset[0][3].shape)
-print("Future Kinematics Shape: ", train_dataloader.dataset[0][4].shape)
-print("Train N Trials: ", train_dataloader.dataset.get_num_trials())
-print("Train Max Length: ", train_dataloader.dataset.get_max_len())
-print("Test N Trials: ", valid_dataloader.dataset.get_num_trials())
-print("Test Max Length: ", valid_dataloader.dataset.get_max_len())
-print("Features: ", train_dataloader.dataset.get_feature_names())
+    #------------------------------------------Build the model and the optimizer---------------------------
 
-# Model Params and Initialization -------------------------------------------------------------------------------------------------
-torch.manual_seed(0)
-emb_size = 64
-nhead = 4
-ffn_hid_dim = 512
-num_encoder_layers = 1
-num_decoder_layers = 1
-decoder_embedding_dim = 8
-num_features = len(train_dataloader.dataset.get_feature_names())
-num_output_classes = len(train_dataloader.dataset.get_target_names())
-max_len = observation_window
+    # Build the Model
+    from models.transformer import TransformerEncoderDecoderModel
+    args = dict(
+        num_encoder_layers = 6,
+        num_decoder_layers = 6,
+        emb_dim = 128,
+        dropout = 0.4,
+        optimizer_type = 'Adam',
+        weight_decay = 0.001,
+        lr = 1e-3,
+        nhead = 4,
+        dim_feedforward = 512
+    )
 
+    model = TransformerEncoderDecoderModel(encoder_input_dim=len(train_dataloader.dataset.get_feature_names()),
+                 decoder_input_dim=len(train_dataloader.dataset.get_target_names()),
+                 num_encoder_layers=args['num_encoder_layers'],
+                 num_decoder_layers=args['num_decoder_layers'],
+                 emb_dim=args['emb_dim'],
+                 nhead=args['nhead'],
+                 tgt_vocab_size=len(train_dataloader.dataset.get_target_names()),
+                 max_encoder_len=observation_window,
+                 max_decoder_len=prediction_window,
+                 decoder_embedding_dim=args[''],
+                 dim_feedforward=args['dim_feedforward'],
+                 dropout=args['dropout'])
+    
+    # Build the optimizer
+    if args['optimizer_type'] == 'Adam':
+        optimizer_cls = torch.optim.Adam
+    elif args['optimizer_type'] == 'AdamW':
+        optimizer_cls = torch.optim.AdamW
+    optimizer = optimizer_cls(model.parameters(), lr=args['lr'], weight_decay=args['weight_decay'])
 
-prediction_transformer = RecognitionModel(encoder_input_dim=num_features,
-                                            decoder_input_dim=num_output_classes, 
-                                            num_encoder_layers=num_decoder_layers,
-                                            num_decoder_layers=num_decoder_layers,
-                                            emb_size=emb_size,
-                                            nhead=nhead,
-                                            tgt_vocab_size=num_output_classes,
-                                            max_len = max_len,
-                                            decoder_embedding_dim = decoder_embedding_dim,
-                                            dim_feedforward=ffn_hid_dim,
-                                            dropout=0.1,
-                                            activation=torch.nn.GELU())
+    #----------------------------------------Training Loop-------------------------------------------------
+    experiment_name = 'transformer_kin_context'
+    results = {}
+    epochs = 20
+    train_losses, valid_losses = [], []
+    for epoch in range(epochs):
+        epoch_train_losses, train_accuracy, train_edit_score = train_model(model, optimizer, train_dataloader)
+        epoch_valid_losses, valid_accuracy, valid_edit_score = eval_model(model, valid_dataloader)
 
-# recognition_transformer = DirectRecognitionModel(encoder_input_dim=num_features,
-#                                             num_encoder_layers=num_decoder_layers,
-#                                             emb_size=emb_size,
-#                                             nhead=nhead,
-#                                             tgt_vocab_size=num_output_classes,
-#                                             max_len = max_len,
-#                                             dim_feedforward=ffn_hid_dim,
-#                                             dropout=0.1)
-recognition_transformer = prediction_transformer.to(device)
-for p in recognition_transformer.parameters():
-    if p.dim() > 1:
-        torch.nn.init.xavier_uniform_(p)
-
-# loss function -------------------------------------------------------------------------------------------------
-loss_fn = torch.nn.CrossEntropyLoss()
-
-# optimizer -------------------------------------------------------------------------------------------------
-optimizer = torch.optim.Adam(recognition_transformer.parameters(), lr=2e-5, betas=(0.9, 0.98), eps=1e-9)
-schd_optim = ScheduledOptim(optimizer, lr_mul=1, d_model=emb_size, n_warmup_steps=2000)
-
-
-# Train / Validation Loops -------------------------------------------------------------------------------------------------
-def train_epoch(model, optimizer, train_dataloader):
-    model.train()
-    losses = 0
-    running_loss = 0.0
-
-    for i, (src, src_image, tgt, future_gesture, future_kinematics) in enumerate(train_dataloader):
-
-        # transpose inputs into the correct shape [seq_len, batch_size, features/classes]
-        src = src.transpose(0, 1) # the srd tensor is of shape [batch_size, sequence_length, features_dim]; we transpose it to the proper dimension for the transformer model
-        tgt = tgt.transpose(0, 1)
-        tgt_input = tgt[:-1, :]
-        
-        # get the target mask
-        tgt_mask = get_tgt_mask(observation_window, device)
-
-        # model outputs
-        # logits = model(src, tgt_input, tgt_mask)
-        logits = model(src)
-
-        optimizer.zero_grad()
-
-        tgt_out = tgt[1:, :]
-        if one_hot:
-            tgt_comp = torch.argmax(tgt_out, dim=-1).reshape(-1)
-        else:
-            tgt_comp = tgt_out.reshape(-1)
-        loss = loss_fn(logits.reshape(-1, logits.shape[-1]), tgt_comp)
-        loss.backward()
-
-        optimizer.step()
-        losses += loss.item()
-
-        # printing statistics
-        running_loss += loss.item()
-        if i % 50 == 0:    # print every 50 mini-batches
-            print(f'[{i + 1:5d}] loss: {running_loss / 50:.3f}')
-            running_loss = 0.0
-
-    return losses / len(list(train_dataloader))
-
-
-def evaluate(model, valid_dataloader):
-    model.eval()
-    losses = 0
-    running_loss = 0.0
-    pred = []
-    gt = []
-    accuracy = 0
-    n_batches = len(valid_dataloader)
-    for src, src_image, tgt, future_gesture, future_kinematics in valid_dataloader:
-
-        src = src.transpose(0, 1)
-        tgt = tgt.transpose(0, 1)
-        tgt_input = tgt[:-1, :]
-
-        tgt_mask = get_tgt_mask(observation_window, device)
-
-        # logits = model(src, tgt_input, tgt_mask)
-        logits = model(src)
-        logits_reshaped = logits.reshape(-1, logits.shape[-1])
-
-        tgt_out = tgt[1:, :]
-        if one_hot:
-            tgt_comp = torch.argmax(tgt_out, dim=-1).reshape(-1)
-            tgt_reshaped = torch.argmax(tgt_out, dim=-1).reshape(-1)
-        else:
-            tgt_comp = tgt_out.reshape(-1)
-            tgt_reshaped = tgt_out.reshape(-1)
-        loss = loss_fn(logits_reshaped, tgt_comp)
-
-        predicted_targets = torch.argmax(logits_reshaped, dim=-1).cpu().detach().numpy()
-        accuracy += np.mean(predicted_targets == tgt_reshaped.cpu().numpy())
-        # print("predictions: ", predicted_targets)
-        # print("ground truth: ", tgt_reshaped.cpu().numpy())
-        pred.append(predicted_targets.reshape(-1))
-        gt.append(tgt_reshaped.cpu().numpy().reshape(-1))
-
-        # print(f"Valid: Accuracy for frame: {accuracy}")
-        losses += loss.item()
-
-    pred, gt = np.concatenate(pred), np.concatenate(gt)
-    print(get_classification_report(pred, gt, train_dataloader.dataset.get_target_names()))
-    print(f"Evaluation accuracy: {accuracy/n_batches}")
-    return losses / len(list(valid_dataloader)), accuracy/n_batches
-
-def cross_validation(model, optimizer, users: List[int], epochs: int):
-    losses, accuracies = list(), list()
-    for user in users:
-        train_dataloader, valid_dataloader = get_dataloaders(
-            tasks,
-            user,
-            observation_window,
-            prediction_window,
-            batch_size,
-            one_hot,
-            class_names = all_class_names,
-            feature_names = feature_names,
-            cast = cast)
-        for epoch in range(1, epochs+1):
-            start_time = timer()
-            train_loss = train_epoch(model, optimizer, train_dataloader)
-            end_time = timer()
-            val_loss, val_accuracy = evaluate(model, valid_dataloader)
-            losses.append(val_loss)
-            accuracies.append(val_accuracy)
-            print((f"Epoch: {epoch}, Train loss: {train_loss:.3f}, Val loss: {val_loss:.3f}, "f"Epoch time = {(end_time - start_time):.3f}s"))
-    # save the model
-
-    # return the main metric
-    return np.mean(losses), np.mean(accuracies)
-
-mean_cv_loss, mean_cv_accuracy = cross_validation(recognition_transformer, optimizer, [2, 3, 4, 5, 6, 8, 9], 2)
-print(mean_cv_loss, mean_cv_accuracy)
-exit()
-
-
-from timeit import default_timer as timer
-NUM_EPOCHS = 10
-
-for epoch in range(1, NUM_EPOCHS+1):
-    start_time = timer()
-    train_loss = train_epoch(recognition_transformer, optimizer)
-    end_time = timer()
-    val_loss = evaluate(recognition_transformer)
-    print((f"Epoch: {epoch}, Train loss: {train_loss:.3f}, Val loss: {val_loss:.3f}, "f"Epoch time = {(end_time - start_time):.3f}s"))
-
-
-# function to generate output sequence using greedy algorithm
-def greedy_decode(model, src, max_len, start_symbol, num_classes):
-
-    model.eval()
-    with torch.no_grad():
-        if not one_hot:
-            ys = torch.ones(1).fill_(start_symbol).to(torch.long).to(device).view(1, 1)
-        else:
-            ys = F.one_hot(ys.to(torch.int64), num_classes).to(torch.float32).to(device).unsqueeze(1)
-        memory = model.encode(src.unsqueeze(1)).to(device)
-
-        for i in range(max_len-1):
-            tgt_mask = get_tgt_mask(ys.shape[0], device)
-            out = model.decode(ys, memory, tgt_mask)
-            prob = model.fc_output(out[-1, :, :])
-            next_word = torch.argmax(prob.view(-1))
-            # next_word = next_word.item()
-            if not one_hot:
-                new_y = next_word.to(torch.long).to(device).view(1, 1)
-            else:
-                new_y = F.one_hot(torch.ones(1).fill_(next_word).to(torch.int64), num_classes).to(torch.float32).reshape(1, 1, -1)
-            ys = torch.cat([ys, new_y], dim=0)
-
-    return ys
-
-max_len = 30
-X_trial, Y_trial = valid_dataloader.dataset.get_trial(0)
-initial_symbol = Y_trial[-max_len-1]
-X_trial, Y_trial = X_trial[-max_len:], Y_trial[-max_len:]
-# pred = greedy_decode(recognition_transformer, X_trial, X_trial.shape[0], initial_symbol, len(valid_dataloader.dataset.get_target_names()))
-pred = recognition_transformer(X_trial.unsqueeze(1))
-print(pred.shape)
-print(Y_trial)
-print(torch.argmax(pred, dim=-1).view(-1))
-# print(torch.argmax(pred2, dim=-1).view(-1))
-# print(torch.argmax(Y_trial, dim=1))
-# print(torch.mean((torch.argmax(pred, dim=-1).view(-1) == torch.argmax(Y_trial, dim=1)).to(torch.float32)))
-
-
-
-
-
-def get_optimal_params():
-    # build an objective based on the cross validation metric
-    # search over the hyper parameters, find the optimal one and save it
-    pass
+        train_losses.append(epoch_train_losses)
+        valid_losses.append(epoch_valid_losses)
+    
+    # save model, accuracy, edit_score, loss-plots for the current subject
+    save_artifacts(model, train_accuracy, valid_accuracy, train_losses, valid_losses)
